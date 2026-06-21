8719d985bdc80b13a6ec0a6bd3881b9340535d70
Vaclav Bohac <bohac.v@gmail.com>
2026-06-21 20:17:33 +0200

    Lobby – removed vector cloning

--- 03-lobby/src/battery.rs
+++ 03-lobby/src/battery.rs
@@ -1,40 +1,32 @@
 pub fn parse_battery(s: &str) -> Vec<u32> {
-    let digits: Vec<u32> = s
-        .chars()
-        .map(|c| c.to_digit(10).unwrap())
-        .collect();
-
-    digits
+    s.chars().map(|c| c.to_digit(10).unwrap()).collect()
 }
 
-pub fn get_joltage(battery: Vec<u32>, cells: u32) -> u64 {
+pub fn get_joltage(battery: &[u32], cells: u32) -> u64 {
     assert!(cells > 0);
     assert!(battery.len() >= cells as usize);
 
-    let mut result: Vec<u64> = Vec::new();
-
-    let mut cloned = battery.clone();
+    let mut result = 0u64;
+    let mut start = 0;
 
     for j in (0..cells).rev() {
-        let mut max_i = 0;
         let mut max = 0;
-        let end = cloned.len() - j as usize;
 
-        for i in 0..end {
-            if cloned[i] > max {
-                max = cloned[i];
-                max_i = i;
+        let end = battery.len() - j as usize;
+
+        for i in start..end {
+            if battery[i] > max {
+                max = battery[i];
+                start = i + 1;
             }
         }
 
         let n = 10u64.pow(j);
 
-        result.push(max as u64 * n);
-
-        cloned = cloned[max_i + 1..cloned.len()].to_vec();
+        result += max as u64 * n;
     }
 
-    result.iter().sum()
+    result
 }
 
 #[cfg(test)]
@@ -47,25 +39,25 @@ mod tests {
 
     #[test]
     fn test_get_joltage_for_single_cell() {
-        let battery = Vec::from([1, 2, 3]);
-        assert_eq!(3, get_joltage(battery, 1));
+        let battery = [1, 2, 3];
+        assert_eq!(3, get_joltage(&battery, 1));
     }
 
     #[test]
     fn test_get_joltage_for_two_cells() {
-        assert_eq!(23, get_joltage(vec![1, 2, 3], 2));
-        assert_eq!(32, get_joltage(vec![3, 2, 1], 2));
+        assert_eq!(23, get_joltage(&[1, 2, 3], 2));
+        assert_eq!(32, get_joltage(&[3, 2, 1], 2));
     }
 
     #[test]
     fn test_get_joltage_for_three_cells() {
-        assert_eq!(123, get_joltage(vec![1, 2, 3], 3));
-        // assert_eq!(321, get_joltage(vec![3, 2, 1], 3));
+        assert_eq!(123, get_joltage(&[1, 2, 3], 3));
+        assert_eq!(321, get_joltage(&[3, 2, 1], 3));
     }
 
     #[test]
     fn test_get_joltage_for_12_cells() {
-        let battery = Vec::from([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1]);
-        assert_eq!(987654321111, get_joltage(battery, 12));
+        let battery = [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1];
+        assert_eq!(987654321111, get_joltage(&battery, 12));
     }
 }
--- 03-lobby/src/main.rs
+++ 03-lobby/src/main.rs
@@ -6,16 +6,16 @@ use crate::battery::{get_joltage, parse_battery};
 fn main() {
     let stdin = io::stdin();
 
-    let mut joltage = 0;
     let mut joltage2 = 0;
+    let mut joltage12 = 0;
 
     for line in stdin.lock().lines() {
         let battery = parse_battery(line.unwrap().trim());
 
-        joltage += get_joltage(battery.clone(), 2);
-        joltage2 += get_joltage(battery.clone(), 12);
+        joltage2 += get_joltage(&battery, 2);
+        joltage12 += get_joltage(&battery, 12);
     }
 
-    println!("{}", joltage);
     println!("{}", joltage2);
+    println!("{}", joltage12);
 }
